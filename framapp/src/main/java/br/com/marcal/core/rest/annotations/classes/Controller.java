cd025ccab378c5015f51530e116e45ee3af978c8
Eduardo Marçal <edumsfilho347@gmail.com>
2025-05-27 14:14:38 -0300

    feat: creating rest annotations

---
+++ framapp/src/main/java/br/com/marcal/core/rest/annotations/classes/Controller.java
@@ -0,0 +1,12 @@
+package br.com.marcal.core.rest.annotations.classes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.TYPE )
+public @interface Controller {
+    String mapping( ) default "";
+}
---
+++ framapp/src/main/java/br/com/marcal/core/rest/annotations/methods/POST.java
@@ -0,0 +1,12 @@
+package br.com.marcal.core.rest.annotations.methods;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+public @interface POST {
+    String endpoint( ) default "";
+}
